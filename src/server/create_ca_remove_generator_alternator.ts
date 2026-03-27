992dc12679d83275352995de9aecfe372c83f0b2
Alex <Agent772@users.noreply.github.com>
2026-03-27 09:44:42 +0100

    Add KubeJS files to remove specific recipes for alternator and electric motor renamed ticket fix to represent the correct mod

---
+++ src/server/create_ca_remove_generator_alternator.ts
@@ -0,0 +1,9 @@
+
+ServerEvents.recipes((event)=>{
+    event.remove({
+        output: 'createaddition:alternator'
+    });
+    event.remove({
+        output: 'createaddition:electric_motor'
+    });
+});
