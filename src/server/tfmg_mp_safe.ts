7367c140ab9db6fd7b23847585f82c0efb2c3d3a
Alex <Agent772@users.noreply.github.com>
2026-03-29 08:54:15 +0200

    Add KubeJS script to remove specific explosive recipes

---
+++ src/server/tfmg_mp_safe.ts
@@ -0,0 +1,21 @@
+
+ServerEvents.recipes((event)=>{
+    event.remove({
+        output: 'tfmg:napalm_bomb'
+    });
+    event.remove({
+        output: 'tfmg:flamethrower'
+    });
+    event.remove({
+        output: 'tfmg:thermite_grenade'
+    });
+    event.remove({
+        output: 'tfmg:zinc_grenade'
+    });
+    event.remove({
+        output: 'tfmg:copper_grenade'
+    });
+    event.remove({
+        output: 'tfmg:pipe_bomb'
+    });
+});
